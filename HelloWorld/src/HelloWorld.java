e1c7441a0900fa21b7347e975d2963c86f7b4712
varsha bakshi <varsha_bakshi@persistent.co.in>
2017-08-30 11:33:50 +0530

    commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//display message on console
+		System.out.println("Hello World");
+	}
+
+}
